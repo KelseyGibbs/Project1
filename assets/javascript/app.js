e63790630cf194d42d6e0603ed5fab272426ad17
Ashley Ricketts <aricketts12@gmail.com>
2019-03-11 13:04:26 -0500

    changed from MATERIZLIZE CSS to BOOTSTRAP

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,13 @@
 
-// date picker from calendar
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.datepicker');
-    var instances = M.Datepicker.init(elems, options);
-  });
+$(document).ready(function(){
+    
+
+
+
+
+
+
+});
+
+
+  
