15e0741079f2a7a61b235b4e5c388198d2ec2199
KelseyGibbs <gibbs.kelseylee@gmail.com>
2019-03-13 18:58:46 -0500

    ticketmaster

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,62 +1,71 @@
 $(document).ready(function () {
 
-    var artist = "Madonna";
-    console.log(artist);
+    // When the submit button is clicked, prevent default and...
+    $(".btn").on("click", function (event) {
+        event.preventDefault();
+        console.log("hey");
 
-    var queryURL = "http://app.ticketmaster.com/discovery/v1/events.json?keyword=" + artist + "&apikey=rbzvFPQuTHwPs9wYmrP99BX332WdMatP&callback=myFunction"
-    console.log(queryURL)
+        // Get the value of the user inputs
+        var dateInput = $("#inputDate").val().trim();
+        var genreInput = $("#inputGenre").val().trim();
+        var priceInput = $("#choosePrice").val();
+        var cityInput = $("#inputCity").val().trim();
 
-    $.ajax({
-        type: "GET",
-        url: queryURL,
-        async: true,
-        dataType: "json",
-        success: function (response) {
+        console.log(priceInput);
+        priceRange = "0," + priceInput;
+        console.log(priceRange);
+        
+        var queryURL = "http://app.ticketmaster.com/discovery/v2/events.json?apikey=rbzvFPQuTHwPs9wYmrP99BX332WdMatP" +
+         "&city=" + cityInput + 
+        "&classificationName=music" + 
+        "&sort=relevance,asc";
 
-            if (response.hasOwnProperty('_embedded')) {
-                console.log("It has property embedded");
-
-
-                for (var i = 0; i < 5; i++) {
-                    console.log(response._embedded.events[i]);
-
-
+        console.log(queryURL)
+        
+        $.ajax({
+            type: "GET",
+            url: queryURL,
+            async: true,
+            dataType: "json",
+            success: function (response) {
+                
+                if (response.hasOwnProperty('_embedded')) {
+                    console.log("It has property embedded");
+                    
+                    
+                    for (var i = 0; i < 5; i++) {
+                        console.log(response._embedded.events[i]);
+                        
+                        
+                    }
+                    
+                    
+                } else {
+                    
+                    console.log("It does NOT have property embedded");
+                    
+                    $("#concertSchedule").html("<em>" + "TicketMaster currently does not have any event information for " + artist + ".</em>");
+                    
+                    
                 }
-
-
-            } else {
-
-                console.log("It does NOT have property embedded");
-
-                $("#concertSchedule").html("<em>" + "TicketMaster currently does not have any event information for " + artist + ".</em>");
-
-
             }
-        }
-    });
-
-
-    var queryURL2 = ""
-    $.ajax({
-        type: "GET",
-        url: queryURL2,
-        async: true,
-        dataType: "json",
-        success: function (response) {
-
-        }
+        });
+        
     })
+        
 
-
-
-
-
-
-
-
-
-
-
-
-
-});
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    });
+    
+    
