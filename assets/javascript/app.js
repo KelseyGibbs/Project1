6bf1d8967e4ff5353955e694956148b7d8a43d87
Ashley Ricketts <aricketts12@gmail.com>
2019-03-13 19:31:09 -0500

    geolocation retrives lat and long

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -30,78 +30,6 @@ $(document).ready(function () {
 
                 $("#concertSchedule").html("<em>" + "TicketMaster currently does not have any event information for " + artist + ".</em>");
 
-$(document).ready(function(){
-    
-
-
-
-
-
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// finding user location
-
-
-  
-=======
-
-// date picker from calendar
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.datepicker');
-    var instances = M.Datepicker.init(elems, options);
-  });
-
-$(document).ready(function(){
-var search = "cheeseburgers"
- 
-    function yelpCall () {
-        var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/WavvLdfdP6g8aZTtbBQHTw",
-            "method": "GET",
-            "headers": {
-              "Authorization": "Bearer QPHJeCyKhtLXLqpFK-oy08dSYsiWJb-OABR-ET1CM__QDP3M2lIaSf-fvas3LIsDgta9ZuTZ5z_cEW1hjI8fBawa5YWr_v5g1alkNrWznK58xqo3mdZcpyJiUf6DXHYx",
-              "cache-control": "no-cache"
-=======
 
             }
         }
@@ -118,6 +46,30 @@ var search = "cheeseburgers"
 
         }
     })
+
+
+
+
+
+
+
+// finding user location
+
+
+var queryLocationURL = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyBroF5LX3P_qRm3VK8IaOGVCaY1g06VhG4",
+    "method": "POST",
+    "headers": {
+      "cache-control": "no-cache",
+      "Postman-Token": "1c36ce3c-b962-42c3-86d5-7deccb5a3fdf"
+    }
+  }
+  
+  $.ajax(queryLocationURL).done(function (response) {
+    console.log(response.location);
+  });
 
 
 
