6dc259e26badc0b8e8647202e4cf5e0ecee9f4fe
Ashley Ricketts <aricketts12@gmail.com>
2019-03-13 19:37:03 -0500

    geolocation code retrieves lat and long

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,60 +1,118 @@
 $(document).ready(function () {
 
-    var artist = "Madonna";
-    console.log(artist);
+    // When the submit button is clicked, prevent default and...
+    $(".btn").on("click", function (event) {
+        event.preventDefault();
+        console.log("hey");
 
-    var queryURL = "http://app.ticketmaster.com/discovery/v1/events.json?keyword=" + artist + "&apikey=rbzvFPQuTHwPs9wYmrP99BX332WdMatP&callback=myFunction"
-    console.log(queryURL)
+        // Get the value of the user inputs
+        var dateInput = $("#inputDate").val().trim();
+        var genreInput = $("#inputGenre").val().trim();
+        var priceInput = $("#choosePrice").val();
+        var cityInput = $("#inputCity").val().trim();
 
-    $.ajax({
-        type: "GET",
-        url: queryURL,
-        async: true,
-        dataType: "json",
-        success: function (response) {
+        console.log(priceInput);
+        priceRange = "0," + priceInput;
+        console.log(priceRange);
 
-            if (response.hasOwnProperty('_embedded')) {
-                console.log("It has property embedded");
+        var queryURL = "http://app.ticketmaster.com/discovery/v2/events.json?apikey=rbzvFPQuTHwPs9wYmrP99BX332WdMatP" +
+            "&city=" + cityInput +
+            "&classificationName=music" +
+            "&sort=relevance,asc";
+
+        console.log(queryURL)
+
+        $.ajax({
+            type: "GET",
+            url: queryURL,
+            async: true,
+            dataType: "json",
+            success: function (response) {
+
+                if (response.hasOwnProperty('_embedded')) {
+                    console.log("It has property embedded");
 
 
-                for (var i = 0; i < 5; i++) {
-                    console.log(response._embedded.events[i]);
+                    for (var i = 0; i < 5; i++) {
+                        console.log(response._embedded.events[i]);
+
+
+                    }
+
+
+                } else {
+
+                    console.log("It does NOT have property embedded");
+
+                    $("#concertSchedule").html("<em>" + "TicketMaster currently does not have any event information for " + artist + ".</em>");
 
 
                 }
+            }
+        });
+
+    })
+
+    $.ajax({
+        type: 'POST',
+        url: 'http://ws.audioscrobbler.com/2.0/',
+        data: 'method=artist.getinfo&' +
+            'artist=Dave+Matthews&' +
+            'getTopTags&' +
+            'getTopTracks&' +
+            'api_key=a365207bc395cbad8267b11acaca3263&' +
+            'format=json',
+        dataType: 'jsonp',
+        success: function (data) {
+            console.log("data from api", data.artist.tags.tag[0].name)
+            // Handle success code here
+            var response = data.data;
+            for (i = 0; i < data.length; i++) {
+
+                var DataDiv = $("<div>");
 
 
-            } else {
+                console.log("making sure this works", results[i].images.fixed_width_still.url);
+                $("#spotifyData").html(data.artist.name)
+                ArtistImage.attr(data.image[0])
 
-                console.log("It does NOT have property embedded");
-
-                $("#concertSchedule").html("<em>" + "TicketMaster currently does not have any event information for " + artist + ".</em>");
-
+                DataDiv.append(p);
+                DataDiv.append(ArtistImage);
 
             }
+            var name = $("<h1>").text(data.artist.name);
+
+            var ArtistImage = $("<img>");
+            ArtistImage.attr("src", data.artist.image[3]["#text"]);
+
+            var p = $("<p>").text(data.artist.tags.tag[0].name);
+            $("#spotifyData").append(name, ArtistImage, p);
         }
+
     });
 
-
-    var queryURL2 = ""
     $.ajax({
-        type: "GET",
-        url: queryURL2,
-        async: true,
-        dataType: "json",
-        success: function (response) {
+        type: 'POST',
+        url: 'http://ws.audioscrobbler.com/2.0/',
+        data: 'method=artist.gettoptracks&' +
+            'artist=Dave+Matthews&' +
+            'getTopTags&' +
+            'getTopTracks&' +
+            'api_key=a365207bc395cbad8267b11acaca3263&' +
+            'format=json',
+        dataType: 'jsonp',
+        success: function (data) {
+            console.log("data from api", data.toptracks.track[0].name);
+            var topTracks = $("<p>").text(data.toptracks.track[0].name);
+            $("#spotifyData").append(data.toptracks.track[0].name);
+
+
 
         }
     })
 
 
-
-
-
-
-
 // finding user location
-
 
 var queryLocationURL = {
     "async": true,
@@ -79,8 +137,21 @@ var queryLocationURL = {
 
 
 
-
-
-
-
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
